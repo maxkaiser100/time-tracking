aa0ca8fc3ba79b149dab52b4df07cb192d65bc00
Florian Jourde <jourdeflorian@gmail.com>
2022-05-17 00:05:52 +0200

    Refactoring JS

--- app.js
+++ app.js
@@ -1,57 +1,49 @@
-fetch('data.json')
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    globalData = data;
-})
-.catch(function (err) {
-    console.log('error: ' + err);
-}); 
-
-
-const timeChoose = document.querySelectorAll('li');
+const timeChoose = document.querySelectorAll('.timeSelect li');
 const previousPer = document.querySelectorAll('.previousPer')
 let sections = document.getElementsByTagName('section');
-let globalTime = '';
-let globalData = '';
 
-//Responds to the click of the timeframe buttons
-timeChoose.forEach(time => {
-  
-  time.addEventListener('click', e => {
-    globalTime = time.innerHTML;
+fetch('data.json')
+.then(function (response) {
+  return response.json();
+})
+.catch(function (err) {
+  console.log('error: ' + err);
+})
+.then(function (data) {
+  // Responds to the click of the timeframe buttons
+  timeChoose.forEach(element => {
+    element.classList.remove('active');
 
-    //Colors the active text button and decolors the non-actives
-    e.target.style.color = "white";
-    time.nextElementSibling.style.color = "#6F76C8";
-    time.previousElementSibling.style.color = "#6F76C8";
-   
-    //Updates the work numbers 
-    for (let i = 0; i < 6; i++) {
-          var currentWorkHrs = sections[i].querySelector('.workHrs');
-          currentWorkHrs.innerHTML = globalData[i].timeframes[globalTime.toLowerCase()].current + 'hrs';
-          var previousHrs = sections[i].querySelector('.workPrev');
-          previousHrs.innerHTML = '&nbsp;' + globalData[i].timeframes[globalTime.toLowerCase()].previous;
-    }
-
-    //Changes the prefix text for the previous hours
-    if(time.innerHTML === 'Daily') {
-      previousPer.forEach(text => {
-        text.innerText = 'Yesterday - ';       
+    element.addEventListener('click', function() {
+      timeChoose.forEach(item => {
+        item.classList.remove('active');
       });
-    };
-    if(time.innerHTML === 'Weekly') {
+
+      element.classList.add('active');
+      let globalTime = element.innerHTML;
+
+      // Updates the work numbers
+      for (let i = 0; i < data.length; i++) {
+        let currentWorkHrs = sections[i].querySelector('.workHrs');
+        let previousHrs = sections[i].querySelector('.workPrev');
+        currentWorkHrs.innerHTML = data[i].timeframes[globalTime.toLowerCase()].current + 'hrs';
+        previousHrs.innerHTML = '&nbsp;' + data[i].timeframes[globalTime.toLowerCase()].previous + 'hrs';
+      }
+
+      // Changes the prefix text for the previous hours
       previousPer.forEach(text => {
-        text.innerText = 'Last Week - ';
+        if(element.innerHTML === 'Daily') {
+            text.innerText = 'Yesterday - ';
+        } else if(element.innerHTML === 'Weekly') {
+            text.innerText = 'Last Week - ';
+        } else if(element.innerHTML === 'Monthly') {
+            text.innerText = 'Last Month - ';
+        }
+      });
     });
-    }
-    if(time.innerHTML === 'Monthly') {
-      previousPer.forEach(text => {
-        text.innerText = 'Last Month - ';
-      });
-    }  
   });
 });
 
-
+window.onload = function() {
+  timeChoose[0].click();
+};
